a336065d36764a4a06ff15a174aff1c3e0d9fa8a
Junjey123-mx <verneljosue@gmail.com>
2026-07-23 17:24:15 -0600

    feat(framebuffer): add reusable framebuffer foundation

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+mod framebuffer;
+
+use framebuffer::Framebuffer;
+use raylib::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let mut framebuffer = Framebuffer::new(160, 120);
+
+    framebuffer.set_background_color(Color::WHITE);
+    framebuffer.clear();
+
+    framebuffer.set_current_color(Color::new(120, 220, 255, 255));
+    framebuffer.point(80, 60);
+
+    framebuffer.render_to_file("baseline.png");
+
+    println!("Reusable framebuffer validated successfully.");
 }
